e89e5c1a44cf8e0883524a48a2d88bda5300740e
Mohamed Ibrahim <55456401+mohamed8270@users.noreply.github.com>
2024-06-07 17:43:05 +0530

    server and http client updated for jobs details

--- lib/data/service/http_client.dart
+++ lib/data/service/http_client.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:code_pay/bindings/models/jobs_data_model.dart';
+import 'package:code_pay/bindings/models/jobs_details_model.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
@@ -18,6 +19,24 @@ class HttpClientFetch extends GetxController {
         List<JobsDataModel> jobsdata =
             jobsres.map((dynamic e) => JobsDataModel.fromJson(e)).toList();
         return jobsdata;
+      } else {
+        throw Exception('Internal Server Error!');
+      }
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+
+  // fetching jobs details using ID
+  Future<JobsDetailsModel> fetchJobsDetails(String id) async {
+    String url = dotenv.env['JOBS_DATA_URL'].toString();
+
+    try {
+      http.Response response = await http.get(Uri.tryParse('$url$id')!);
+      if (response.statusCode == 200) {
+        final jobsdetailres = jsonDecode(response.body);
+        final jobsdetaildata = JobsDetailsModel.fromJson(jobsdetailres);
+        return jobsdetaildata;
       } else {
         throw Exception('Internal Server Error!');
       }
