73319e40580b7fdd6987941075d04ed0904c245f
Mohamed Ibrahim <55456401+mohamed8270@users.noreply.github.com>
2024-08-19 11:14:37 +0530

    route in wrapper updated

--- lib/routes/wrapper/wrapper.dart
+++ lib/routes/wrapper/wrapper.dart
@@ -6,6 +6,7 @@ import 'package:code_pay/data/bloc/news_data/news_data_bloc.dart';
 import 'package:code_pay/routes/pages/home_page/home_page.dart';
 import 'package:code_pay/routes/pages/jobs_page/jobs_page.dart';
 import 'package:code_pay/routes/pages/job_details_page/jobs_details_page.dart';
+import 'package:code_pay/routes/pages/news_page/news_page.dart';
 import 'package:code_pay/routes/pages/scope_page/scope_page.dart';
 import 'package:code_pay/routes/pages/settings_page/settings_page.dart';
 import 'package:flutter/material.dart';
@@ -36,6 +37,7 @@ class Wrapper extends StatelessWidget {
           '/homepage': (c) => const HomePage(),
           '/jobspage': (c) => const JobsPage(),
           '/settingspage': (c) => const SettingsPage(),
+          '/newspage': (c) => const NewsPage(),
           '/scopespage': (c) => const ScopePage(),
           '/jobdetails': (c) => const JobsDetailsPage(),
         },
